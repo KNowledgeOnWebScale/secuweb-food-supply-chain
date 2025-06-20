80709966223a6be5aee7badf3d1c12d5dde02b81
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-06-20 10:03:57 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import {FromSchema} from "json-schema-to-ts";
+
+console.log('index.ts')
+import fastify from 'fastify'
+import {Static, Type} from '@sinclair/typebox'
+
+const ActorID = Type.String()
+const ProductID = Type.String()
+const Location = Type.String()
+const ProductState = Type.Object({
+  // productId: ProductID,
+  quantity: Type.BigInt(),
+  status: Type.String(), // currently we use Type.String(), TODO (later): use Enum
+  // sender: ActorID,
+  // recipient: ActorID,
+  // from: Location,
+  // to: Location,
+})
+
+const ProductSpec = Type.Object({
+  productId: ProductID,
+  name: Type.String(),
+  foodType: Type.String(), // e.g., fruit, vegetable, meat, poultry
+})
+
+const server = fastify()
+const querySchema = {
+  type: 'object',
+  properties: {
+    productId: {type: 'string'}
+  },
+  required: ['productId']
+} as const;
+
+type QueryType = FromSchema<typeof querySchema>
+
+server.get<{Querystring: QueryType}>('/product', {
+  schema: { querystring: querySchema }
+}, async (request, reply) => {
+  console.log(request.url)
+  const {productId} = request.query
+  console.log('productId', productId)
+  // const { q, page } = request.query;
+  // return { query: q, page };
+  return {productId}
+});
+
+
+server.post('/product', { schema: { body: ProductSpec }},
+  async (request, reply) => {
+  return 'TODO'
+})
+
+// server.post('/productstate', { schema: { body: ProductState }},
+//   async (request, reply) => {
+//   return 'TODO'
+// })
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
