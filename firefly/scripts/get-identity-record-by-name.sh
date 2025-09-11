fa41f910c48e87bf4bf169e6bd0fdff51a601ac2
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-09-12 00:20:43 +0200

    updated documentation; added error checking

--- firefly/scripts/get-identity-record-by-name.sh
+++ firefly/scripts/get-identity-record-by-name.sh
@@ -1,10 +1,14 @@
 # !/bin/bash
-# get-org-uuid-by-name.sh
-# This script retrieves the UUID of an organization by its name from the identities.json file.
+# get-identity-record-by-name.sh
+# This script retrieves an identity record by name from the identities.json file.
 set -e
 set -u
 set -o pipefail
-
+FPATH_IDENTITIES_JSON="identities.json"
+if [ ! -f "$FPATH_IDENTITIES_JSON" ]; then
+  echo "❌ Error: $FPATH_IDENTITIES_JSON file not found!"
+  exit 1
+fi
 NAME=$1
 SELECTOR='.[] | select(.name == "'$NAME'")'
-jq "$SELECTOR" identities.json
+jq "$SELECTOR" "$FPATH_IDENTITIES_JSON"
