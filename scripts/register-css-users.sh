6f0e08cbf010efbd796509c62f04e15dbdb965f1
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-09-12 13:36:20 +0200

    update

--- scripts/register-css-users.sh
+++ scripts/register-css-users.sh
@@ -5,16 +5,21 @@
 DIR_FF_SCRIPTS='./firefly/scripts'
 FPATH_CSS_USERS="./css/css-users.json"
 
-STACK="dev2"
+STACK="dev"
 echo "Currently, stack is fixed to: $STACK"
 HOST=http://localhost:10000
 echo "Currently, host is fixed to: $HOST"
 
 USERNAMES=$(jq '[ .[] |  .pods[0].name ]' "$FPATH_CSS_USERS")
-echo "Registering the following CSS users on Firefly: $USERNAMES"
+pods=($(jq -r '.[].pods[0].name' $FPATH_CSS_USERS))
+echo "Pods array: ${pods[@]}"
+#pods=("${pods[@]1:}")
 
-for USERNAME in $(echo "$USERNAMES" | jq -r '.[]'); do
+
+for pod in "${pods[@]}"; do
+  USERNAME=$pod
   echo "➡️ Registering user: $USERNAME"
+  
   # Extract email and password for the user from the JSON file
   EMAIL=$(jq -r '.[] | select(.pods[].name=="'$USERNAME'") | .email' "$FPATH_CSS_USERS")
   PASSWORD=$(jq -r '.[] | select(.pods[].name=="'$USERNAME'") | .password' "$FPATH_CSS_USERS")
@@ -25,14 +30,33 @@ for USERNAME in $(echo "$USERNAMES" | jq -r '.[]'); do
   echo "➡️ Create and register keypair for user $USERNAME"
   ./create-register-key.sh $STACK $USERNAME
   
+  # Fetch identities again to ensure we have the latest data
+  sleep 5
+  ./get-identities.sh
+  
+  echo "Identities:"
+  jq '.[] | .name' identities.json
+
   echo "Identity record for user $USERNAME:"
   # Extract the DID from the identity record
   FF_ID_RECORD=$(./get-identity-record-by-name.sh $USERNAME)
+  echo "$FF_ID_RECORD"
+  # Validate that FF_ID_RECORD is not empty
+  if [ -z "$FF_ID_RECORD" ]; then
+    echo "❌ Error: No identity record found for user $USERNAME."
+    exit 1
+  fi
   FF_DID=$(echo "$FF_ID_RECORD" | jq .did)
   cd -
 
   echo "➡️ Updating WebID profile for user $USERNAME with Firefly DID: $FF_DID"
+  # Check if FF_DID is empty or null
+  if [ -z "$FF_DID" ] || [ "$FF_DID" == "null" ]; then
+    echo "❌ Error: Firefly DID is empty or null for user $USERNAME."
+    exit 1
+  fi
   # Add the Firefly DID to the user's WebID profile
   npm run add-firefly-did-to-webid-profile -- --name $USERNAME --email "${EMAIL}" --password "${PASSWORD}" --firefly-did "${FF_DID}"
+  
 done
 
