75cb9cb3a2f5255672ad2af1440e13bfe6c89f72
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-09-15 15:42:09 +0200

    set error handling flags

--- firefly/scripts/get-key-address.sh
+++ firefly/scripts/get-key-address.sh
@@ -3,6 +3,8 @@
 # Retrieves the address of a key from a given key file.
 # Usage: ./get-key-address.sh <key_file>
 set -e
+set -o pipefail
+
 KEYFILE="$1"
 if [ -z "$KEYFILE" ]; then
   echo "⚠️ Usage: $0 <key_file>"
