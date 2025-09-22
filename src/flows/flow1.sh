6a9d3d9be27adbb9b81c5cc22769e696e324b394
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-09-23 01:56:38 +0200

    update: flow1.sh

--- src/flows/flow1.sh
+++ src/flows/flow1.sh
@@ -1,7 +1,8 @@
+# This script creates a verifiable credential from a data file and writes it to a Solid Pod.
 _USERNAME=farmer
 _EMAIL="info@farmer.com"
 _PASSWORD=farmer123
-_FPATH_DATA='./examples/farmer/shipment.json'
+_FPATH_DATA='./src/flows/data/farmer/shipment.json'
 _FPATH_DATA_VC='./src/flows/output/shipment1-vc.jsonld'
 _CONTAINER='shipments'
 # Create a verifiable credential from the data file
