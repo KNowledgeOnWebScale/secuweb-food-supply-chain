d4f13b7969eb74f6649e62fd0a3aec36007d826a
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-09-23 02:08:52 +0200

    update: (#13) original data files are stored separate from their VC variant (#10)

--- src/flows/flow1.sh
+++ src/flows/flow1.sh
@@ -5,8 +5,14 @@ _PASSWORD=farmer123
 _FPATH_DATA='./src/flows/data/farmer/shipment.json'
 _FPATH_DATA_VC='./src/flows/output/shipment1-vc.jsonld'
 _CONTAINER='shipments'
+_CONTAINER_VC="${_CONTAINER}/vc"
+
+# Write the original data to the Solid Pod
+npm run flows:add-file-to-solid-pod -- --name $_USERNAME --email $_EMAIL --password $_PASSWORD --container $_CONTAINER --inputFile $_FPATH_DATA 
+
+
 # Create a verifiable credential from the data file
 npm run flows:create-vc -- --name $_USERNAME --email $_EMAIL --password $_PASSWORD --data $_FPATH_DATA --output $_FPATH_DATA_VC
 
 # Write the verifiable credential to the Solid Pod
-npm run flows:add-file-to-solid-pod -- --name $_USERNAME --email $_EMAIL --password $_PASSWORD --container $_CONTAINER --inputFile $_FPATH_DATA_VC 
+npm run flows:add-file-to-solid-pod -- --name $_USERNAME --email $_EMAIL --password $_PASSWORD --container $_CONTAINER_VC --inputFile $_FPATH_DATA_VC 
