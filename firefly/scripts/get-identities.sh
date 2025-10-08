54b846e76946813c9d9c1153d9e699894bbb72fb
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-10-08 19:39:11 +0200

    update: output filepath became optional parameter

--- firefly/scripts/get-identities.sh
+++ firefly/scripts/get-identities.sh
@@ -4,11 +4,19 @@ set -e
 set -u
 set -o pipefail
 
-FPATH_OUTPUT=$1
+FPATH_OUTPUT="${1:-}"  # optional first argument
 HOST="http://localhost:10000"
-# Fetches all identities from a Firefly instance.
+
 echo "Fetching all identities from Firefly at $HOST"
 API_URL="${HOST}/api/v1/identities?fetchverifiers=true"
-curl -s -X GET "$API_URL" \
-  -H "Content-Type: application/json" \
-  > $FPATH_OUTPUT
+
+if [[ -n "$FPATH_OUTPUT" ]]; then
+  # If an output file path is provided, write to it
+  curl -s -X GET "$API_URL" \
+    -H "Content-Type: application/json" \
+    > "$FPATH_OUTPUT"
+else
+  # Otherwise, print to console
+  curl -s -X GET "$API_URL" \
+    -H "Content-Type: application/json" | jq .
+fi
