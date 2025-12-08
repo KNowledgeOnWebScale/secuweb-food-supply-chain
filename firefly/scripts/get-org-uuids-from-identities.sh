58ebf143941074988119426917ba30b27ca37144
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-12-08 21:35:27 +0100

    removed firefly related resources and occurrences

--- firefly/scripts/get-org-uuids-from-identities.sh
+++
@@ -1,7 +0,0 @@
-# This script extracts organizations' UUIDs from the identities (JSON) file.
-# Usage: ./get-org-uuids-from-identities.sh <path to identities file>
-set -e
-set -u
-set -o pipefail
-
-jq '[.[] | select(.type == "org") | {id, type, name}]' $1
