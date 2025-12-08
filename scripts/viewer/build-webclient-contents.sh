4d082b7f06ba7d22bcdd1f2aafc2437c2affe878
Gertjan De Mulder <gertjan.demulder@ugent.be>
2025-12-08 21:20:51 +0100

    temp change: viewer repo pulled from internal gitlab repo mirror

--- scripts/viewer/build-webclient-contents.sh
+++ scripts/viewer/build-webclient-contents.sh
@@ -22,7 +22,9 @@ echo $PWD
 
 echo Cloning, selecting tag, installing...
 rm -rf ${VIEWER_CLONE}
-git clone https://github.com/SolidLabResearch/${VIEWER_REPO}.git -b v2.1.0 ${VIEWER_CLONE}
+#git clone https://github.com/SolidLabResearch/${VIEWER_REPO}.git -b v2.1.0 ${VIEWER_CLONE}
+echo "⚠️ Using GitLab SecuWeb fork of the viewer for the FSC demo"
+git clone git@gitlab.ilabt.imec.be:KNoWS/projects/SecuWeb/miravi-a-linked-data-viewer.git -b secuweb/fsc-demo-20251114 ${VIEWER_CLONE}
 cd ${VIEWER_CLONE}/main
 npm install
 
