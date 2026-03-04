e01a7a8cea4d62f9dc98734dd77ff03e3391b1fd
Gertjan De Mulder <gertjan.demulder@ugent.be>
2026-03-04 01:15:42 +0100

    Added Viewer dev safeguard: Added SAFEGUARD_VIEWER_CLONE to prevent wiping Miravi clone during setup

--- scripts/viewer/build-webclient-contents.sh
+++ scripts/viewer/build-webclient-contents.sh
@@ -3,27 +3,45 @@
 # Note: this script assumes:
 #   - to run it the directory where it is located
 #   - our repo is cloned in a directory $OUR_CLONE
-
 set -euo pipefail
 
 OUR_CLONE=food-supply-chain
 VIEWER_REPO=miravi-a-linked-data-viewer
 VIEWER_CLONE=food-supply-chain-miravi
+VIEWER_CLONE_URI="git@gitlab.ilabt.imec.be:KNoWS/projects/SecuWeb/demonstrator/miravi-a-linked-data-viewer.git"
+VIEWER_CLONE_BRANCH="secuweb/fsc-demo-20251114"
+# FLAGS
+SAFEGUARD_VIEWER_CLONE=false
 
 # absolute dir
 OUR_ROOT_DIR=$(pwd)/../..
 
+# Helper
+function clone() {
+    echo "Cloning $VIEWER_REPO into $VIEWER_CLONE..."
+    git clone $VIEWER_CLONE_URI -b $VIEWER_CLONE_BRANCH ${VIEWER_CLONE}
+}
 
-
+#
 pushd ${OUR_ROOT_DIR}/.. > /dev/null
+if [ -d "$VIEWER_CLONE" ]; then
+    if [ "$SAFEGUARD_VIEWER_CLONE" = true ] ; then
+        echo "
+        ⚠️ Directory $VIEWER_CLONE already exists.
+        Thus, it will not be deleted and overridden.
+        Your setup will use the existing directory ($(basename $VIEWER_CLONE)).
+        If you want to override it, please set SAFEGUARD_VIEWER_CLONE=false to proceed anyway;
+        This will delete the existing directory $VIEWER_CLONE and all its contents,
+        before cloning the viewer repository again.
+        "
+    else
+        echo "⚠️ Removing existing directory $VIEWER_CLONE since SAFEGUARD_VIEWER_CLONE is false."
+        rm -rf $VIEWER_CLONE
+        clone;
+    fi
+fi
 
-echo $PWD
-
-
-echo Cloning, selecting tag, installing...
-rm -rf ${VIEWER_CLONE}
-echo "⚠️ Using SecuWeb fork of the viewer for the FSC demo"
-git clone git@github.com:gertjandemulder/miravi-a-linked-data-viewer.git -b secuweb/fsc-demo-20251114 ${VIEWER_CLONE}
+echo "Installing ${VIEWER_CLONE}/main"
 cd ${VIEWER_CLONE}/main
 npm install
 
